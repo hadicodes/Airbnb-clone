ca5fe38da63c1c13db8dc00a53ad718b3ef4e6a4
Hadi Yousufi <reach.hadiy@gmail.com>
2018-02-26 18:34:06 -0600

    Adds the basic react app

---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+// import React class from the react package
+import React from "react";
+
+class Hello extends React.Component {
+    render() {
+        // todo
+        return <div>Hello from hello.js</div>;
+    }
+}
+
+export default Hello;
